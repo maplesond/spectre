b63540a9c2a89929bbdb47a082511234202df761
Daniel Mapleson <daniel.mapleson@tgac.ac.uk>
2014-02-07 14:19:14 +0000

    Reverted handling of output directory from SuperQ CLI.  Added check in SuperQ so that we check if the parent directory of the specified output exists before trying to create it.  If it can't be found then assume the user wants to output the file into the current working directory (so the output directory must already exist).

--- qtools/src/main/java/uk/ac/uea/cmp/phybre/qtools/superq/SuperQCLI.java
+++ qtools/src/main/java/uk/ac/uea/cmp/phybre/qtools/superq/SuperQCLI.java
@@ -111,7 +111,7 @@ public class SuperQCLI {
         }
 
         if (commandLine.hasOption(OPT_OUTPUT)) {
-            sqOpts.setOutputFile(new File(System.getProperty("user.dir"),commandLine.getOptionValue(OPT_OUTPUT)));
+            sqOpts.setOutputFile(new File(commandLine.getOptionValue(OPT_OUTPUT)));
         } else {
             throw new ParseException("You must specify an output file.");
         }
