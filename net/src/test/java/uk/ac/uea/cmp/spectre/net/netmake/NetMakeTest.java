9b6bb89730a8dfd6cc2b768b51850966cf6da13a
Dan <d.mapleson@gmail.com>
2017-03-22 20:52:32 +0000

    Updating NetmakeTests to use newer version of junit.

--- net/src/test/java/uk/ac/uea/cmp/spectre/net/netmake/NetMakeTest.java
+++ net/src/test/java/uk/ac/uea/cmp/spectre/net/netmake/NetMakeTest.java
@@ -36,7 +36,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
-import static junit.framework.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 
 public class NetMakeTest {
